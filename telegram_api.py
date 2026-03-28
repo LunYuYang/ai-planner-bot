30bc24aa36e8e89e9e36a624feab4dce91721250
LunYuYang <randy50114@gmail.com>
2026-03-28 20:04:26 +0800

    Update telegram_api.py

--- telegram_api.py
+++ telegram_api.py
@@ -5,11 +5,7 @@ from config import BOT_TOKEN
 def send_message(chat_id, text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    payload = {
     requests.post(url, json={
         "chat_id": chat_id,
         "text": text
-    }
-
-    requests.post(url, json=payload)
     })
